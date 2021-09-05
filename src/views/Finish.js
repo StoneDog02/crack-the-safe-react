a1b5235f1bb3e7c5749e233fc8b73f7665052b99
Brian Bartholomew <me@bcbrian.com>
2021-09-04 21:09:58 -0700

    initial commit

--- src/views/Finish.js
+++ src/views/Finish.js
@@ -17,14 +17,14 @@ export function Finish({ setView, finishMsg }) {
   return (
     <div className="game-container">
       <SafeDial current={random} numOfOptions={40} />
-      <h1>{finishMsg.title}</h1>
-      <h3 id="result">{finishMsg.subtitle}</h3>
+      {/* <h1>{finishMsg.title}</h1>
+      <h3 id="result">{finishMsg.subtitle}</h3> */}
       <button className="again" onClick={() => setView(VIEWS.PLAY)}>
         play again?
       </button>
-      <button className="nope" onClick={() => setView(VIEWS.START)}>
+      {/* <button className="nope" onClick={() => setView(VIEWS.START)}>
         I dont want to play again
-      </button>
+      </button> */}
     </div>
   );
 }
